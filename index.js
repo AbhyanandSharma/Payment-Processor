21bd8d17993b8cca2884011248bc794da5dae5c6
AbhyanandSharma <122311730+AbhyanandSharma@users.noreply.github.com>
2024-10-26 22:47:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const { sequelize } = config;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin : '*',
+}));
 
 // API Routes
 app.use('/api/payment', paymentRoutes);  // Payment processing endpoints
